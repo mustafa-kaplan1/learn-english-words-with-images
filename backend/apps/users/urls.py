cf2ceacd2bafba1fe4a9a07cab0f5ccd42e4294a
Mustafa Kaplan <mkaplan2005@gmail.com>
2026-06-12 13:35:21 +0300

    user models, urls, views and serializers added

---
+++ backend/apps/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import RegisterView, ProfileView
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+]
